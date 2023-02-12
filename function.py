b190acbe2bf757a5bad6de5c430255bdbf4ab7b2
Quraysh-yaseen <qurayshyaseen2501@gmail.com>
2023-02-12 12:19:54 +0530

    func

---
+++ function.py
@@ -0,0 +1,13 @@
+# A function in Python is defined with the def keyword, followed by the function names, zero or more arguments or named arguments contained in parenthesis (), and a colon : to indicate the start of the function.
+# A Basic Function that accepts no arguments and returns nothing.
+def hello_world():
+    print("Hello, World!")
+
+
+# A Function that accepts two arguments, and returns a value
+def add_numbers(x, y):
+    print(x + y)
+
+
+hello_world()
+add_numbers(2,3)
